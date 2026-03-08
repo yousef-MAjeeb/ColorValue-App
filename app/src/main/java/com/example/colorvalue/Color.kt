d77c180b82fa5df2b2c346387c4eb15e7fefc398
idiot-Dev <3amkhero@gmail.com>
2026-03-09 00:05:40 +0200

    update table Name

--- app/src/main/java/com/example/colorvalue/Color.kt
+++ app/src/main/java/com/example/colorvalue/Color.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "colors")
+@Entity(tableName = "colors_table")
 data class Color(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "hex_color") val hex: String,
